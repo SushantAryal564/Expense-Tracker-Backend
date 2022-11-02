451b23cc1090d316dddc553ecad1d26722ce0bcb
sushant <aryalsushant564@gmail.com>
2022-11-02 08:33:47 +0545

    Error Handling for development environment and production environment

--- Controller/errorController.js
+++ Controller/errorController.js
@@ -1,9 +1,31 @@
-module.exports = (err, req, res, next) => {
-  console.log(err);
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
+const devError = (res, err) => {
   res.status(err.statusCode).json({
+    error: err,
     status: err.status,
     message: err.message,
+    errStack: err.stack,
   });
+};
+const proError = (res, err) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  console.log(err);
+  if (process.env.NODE_ENV == "development") {
+    devError(res, err);
+  }
+  if (process.env.NODE_ENV == "production") {
+    devError(res, err);
+  }
 };
