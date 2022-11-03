e917fccdecd024375181543445dcc09a4473c2d0
sushant <aryalsushant564@gmail.com>
2022-11-03 06:11:25 +0545

    Added JWT for authentication and token is automatically created when user is logged in

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,10 +1,21 @@
 const catchAsync = require("./../utils/catchAsync");
 const User = require("./../Model/usersModel");
-
+const jwt = require("jsonwebtoken");
 exports.signUp = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    email: req.body.email,
+    name: req.body.name,
+    age: req.body.age,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    created_at: req.body.created_at,
+  });
+  const token = jwt.sign({ id: this._id }, process.env.JSON_WEBTOKEN_SECRET, {
+    expiresIn: process.env.JSON_WEBTOKEN_EXPIRES,
+  });
   res.status(200).json({
     status: "success",
+    token,
     user: newUser,
   });
 });
